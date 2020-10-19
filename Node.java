ff231e74c167d3c4ccdd9cb92b333ba333c61bfa
jatin0002 <56964396+jatin0002@users.noreply.github.com>
2020-10-20 00:20:09 +0530

    some corrections

--- Node.java
+++ Node.java
@@ -21,38 +21,57 @@ class Node {
         }
     }
 
+    public static void display(Node node) {
+        String str = "";
+        if (node == null) {
+            return;
+        }
+        str += node.left == null ? "." : node.left.data + "";
+        str += "<-- " + node.data + "--> ";
+        str += node.right == null ? "." : node.right.data + "";
+        System.out.println(str);
+        display(node.left);
+        display(node.right);
+    }
+
     public static void main(String[] args) {
         Integer[] arr = { 50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null,
                 null };
+
         Node root = new Node(arr[0], null, null);
         Pair rootPair = new Pair(root, 1);
 
         Stack<Pair> st = new Stack<>();
         st.push(rootPair);
+
         int idx = 0;
         while (st.size() > 0) {
             Pair top = st.peek();
             if (top.state == 1) {
                 idx++;
-                if (top.node.left != null) {
+                if (arr[idx] != null) {
                     top.node.left = new Node(arr[idx], null, null);
                     Pair leftPair = new Pair(top.node.left, 1);
                     st.push(leftPair);
                 } else {
                     top.node.left = null;
                 }
+                top.state++;
             } else if (top.state == 2) {
                 idx++;
-                if (top.node.right != null) {
+                if (arr[idx] != null) {
                     top.node.right = new Node(arr[idx], null, null);
                     Pair rightPair = new Pair(top.node.right, 1);
                     st.push(rightPair);
                 } else {
                     top.node.left = null;
                 }
+                top.state++;
             } else {
                 st.pop();
             }
         }
+
+        display(root);
     }
 }
